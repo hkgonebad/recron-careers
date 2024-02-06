36ca2f95fac529b1d55f70f2555760108c6c42ae
hkgonebad <harry007007@gmail.com>
2024-02-06 19:13:12 +0530

    new: jobs and job-detail components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+// import { HomeComponent } from './pages/home/home.component';
+import { JobsComponent } from './pages/candidate/jobs/jobs.component';
 
 export const routes: Routes = [
     {
         path: '',
         title: 'Recron Malaysia Careers',
-        component: HomeComponent
-    }
+        component: JobsComponent
+    },
 ];
