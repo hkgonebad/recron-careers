3cc1dab0d2c5dcc4a4a761a16e52ee1f7eb75a02
hkgonebad <harry007007@gmail.com>
2024-02-07 16:05:53 +0530

    start: job listing component

--- src/app/pages/candidate/jobs/jobs.component.ts
+++ src/app/pages/candidate/jobs/jobs.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { remixStarFill, remixMapPin2Fill } from '@ng-icons/remixicon';
+
 @Component({
   selector: 'app-jobs',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
   templateUrl: './jobs.component.html',
-  styleUrl: './jobs.component.scss'
+  styleUrl: './jobs.component.scss',
+  providers: [provideIcons({remixStarFill, remixMapPin2Fill})]
 })
 export class JobsComponent {
 
